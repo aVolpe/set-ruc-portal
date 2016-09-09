f75d5b1b80985bdf1b134bf23787eb165b0810af
Arturo Volpe <arturovolpe@gmail.com>
2016-09-09 20:37:35 +0000

    Componente que permite obtener digitos verificadores
    Componente que permite obtener el digito verificador de un RUC en
    particular.

---
+++ client/src/validator/Validator.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './Validator.css';
+import DigitGenerator from './DigitGenerator';
+
+class Validator extends Component {
+  
+  getValues = (ruc) => {
+    return {
+      ruc : ruc,
+      digit : new DigitGenerator().getDigitoVerificadorBase11(ruc)
+    };
+  }
+  
+  changeCurrentRuc = (evt) => {
+    this.setState(this.getValues(evt.target.value));
+  };
+  
+  state = this.getValues('4787587');
+  
+  render() {
+    return (
+      <div>
+        <h1>Digitos verificadores</h1>
+        <form>
+          <label>Deseo obtener el digito validador de
+            <input 
+              className="ruc-input"
+              placeholder={"1123456, 01-2140"} 
+              type="text" 
+              value={this.state.ruc}
+              onChange={this.changeCurrentRuc} />
+          </label>
+        </form>
+        <label>El dígito de {this.state.ruc} es {this.state.digit}</label>
+      </div>
+    );
+  }
+}
+
+export default Validator;
