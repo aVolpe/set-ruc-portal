5fe9974128a9cfd7febf1e5e8b1a503f34929754
Arturo Volpe <arturovolpe@gmail.com>
2016-09-12 04:05:56 +0000

    Simple module to perform queries to the ruc data
    A simple module that performs queries to the ruc data.
    
    It uses sqlite, in order to user it we need to read all the data
    and stores in a sqlite database, for this we has two helper methods,
    like:
    
    	var helper = require('./db_helper');
    	helper.hasData(function(hasData) {
    		if (!hasData) helper.loadDataFromSet(useIt);
    		else useIt();
    	}
    
    Where useIt is a function that perfom some queries, the helper
    module also provide a simple function `findByRuc` that allows
    the user to get data by the ruc (max 3 rows).

---
+++ db_helper.js
@@ -0,0 +1,81 @@
+var ex = module.exports = {};
+
+var data = ex.data = {
+    dbName : 'db.db',
+    tableName : 'rucs',
+    maxResults : 3
+};
+
+ex.init = function() {
+    
+    data.sqlite = require('sqlite3');
+    data.db = new data.sqlite.Database(data.dbName);
+    data.loaded = true;
+};
+
+/**
+ * The callback receives true if the
+ * database has elements, or else false.
+ */
+ex.hasData = function(callback) {
+    if (!data.loaded) ex.init();
+    
+    data.db.get('SELECT * FROM ' + data.tableName + ' LIMIT 1',
+        function (err, count) {
+            if (err) callback(false);
+            else callback(true);
+        });
+};
+
+/**
+ * Uses the set_parser to get all the initial
+ * data and stores it in the database
+ */
+ex.loadDataFromSet = function(success) {
+    if (!data.loaded) ex.init();
+    var db = data.db;
+    
+    var set_parser = require('./set_parser.js');
+    
+    db.run('CREATE TABLE ' + data.tableName + ' (doc TEXT, name TEXT, div TEXT, old TEXT)', [],  function() {
+      var stmt = db.prepare('INSERT INTO ' + data.tableName + ' VALUES ($doc, $name, $div, $old)');
+        
+      set_parser.readByRow(function newLine(row) {
+         stmt.run(row);
+      }, function endStream() {
+         stmt.finalize(success);
+      });
+    });
+};
+
+/**
+ * Queries the database with the given ruc
+ */
+ex.findByRuc = function(query, success) {
+    if (!data.loaded) ex.init();
+    
+    data.db.all('SELECT * FROM ' + data.tableName + ' WHERE doc LIKE $doc LIMIT $size', 
+        { 
+            $doc : '%' + query + '%',
+            $size : data.maxResults
+        },
+        function(err, rows) {
+            if (!err) return success(rows);
+            // console.log(err);
+        });
+};
+
+ex.init();
+ex.hasData(function(hasData) {
+    function doQuery() {
+        ex.findByRuc('99', function(data) {
+            console.log(data);
+        });
+    }
+    
+    if (!hasData) { 
+        console.log('loading data...');
+        ex.loadDataFromSet(doQuery);
+    }
+    else doQuery();
+})
