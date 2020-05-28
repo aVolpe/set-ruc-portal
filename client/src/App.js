820be313f9f2f1205a9204331eb89a72c804976d
Arturo Volpe <arturovolpe@gmail.com>
2020-05-28 18:43:20 +0000

    feat: Upgrade deps and modernize code
    Update all libraries, modernize the code and fix some issues.

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,101 @@
 import React, { Component } from 'react';
-import './util/pure.css';
 import './App.css';
-
-import { Link, IndexLink } from 'react-router'
+import Validator from './validator/Validator'
+import { Search } from './search/Search'
+import Download from './download/Download'
+import License from './License'
+import About from './About'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+    Redirect
+} from "react-router-dom";
 
 export default class App extends Component {
 
-  initPure = function (window, document) {
+    initPure = function (window, document) {
 
-    var layout   = document.getElementById('layout'),
-        menu     = document.getElementById('menu'),
-        menuLink = document.getElementById('menuLink');
+        const layout = document.getElementById('layout'),
+            menu = document.getElementById('menu'),
+            menuLink = document.getElementById('menuLink');
 
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
+        function toggleClass(element, className) {
+            var classes = element.className.split(/\s+/),
+                length = classes.length,
+                i = 0;
 
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
+            for (; i < length; i++) {
+                if (classes[i] === className) {
+                    classes.splice(i, 1);
+                    break;
+                }
+            }
+            // The className is not found
+            if (length === classes.length) {
+                classes.push(className);
+            }
+
+            element.className = classes.join(' ');
         }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
 
-        element.className = classes.join(' ');
+        menuLink.onclick = function (e) {
+            var active = 'active';
+
+            e.preventDefault();
+            toggleClass(layout, active);
+            toggleClass(menu, active);
+            toggleClass(menuLink, active);
+        };
+
     }
 
-    menuLink.onclick = function (e) {
-        var active = 'active';
+    componentDidMount() {
+        this.initPure(window, document);
+    }
 
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
-    };
+    render() {
+        return (
+            <Router>
+                <div id="layout">
+                    <a href="#menu" id="menuLink" className="menu-link">
+                        <span></span>
+                    </a>
+                    <div id="menu">
+                        <div className="pure-menu">
+                            <span className="pure-menu-heading"></span>
 
-  }
+                            <ul className="pure-menu-list">
+                                <li className="pure-menu-item"><NavLink activeClassName="pure-menu-selected" className="pure-menu-link" to="/search">Buscador</NavLink></li>
+                                <li className="pure-menu-item"><NavLink activeClassName="pure-menu-selected" className="pure-menu-link" to="/validate">Validador</NavLink></li>
+                                <li className="pure-menu-item"><NavLink activeClassName="pure-menu-selected" className="pure-menu-link" to="/download">Descargar</NavLink></li>
+                            </ul>
 
-  componentDidMount() {
-    this.initPure(window, document);
-  }
+                            <div className="bottom-menu">
+                                <ul className="pure-menu-list">
+                                    <li className="pure-menu-item menu-item-divided"><NavLink activeClassName="pure-menu-selected" className="pure-menu-link" to="about">Acerca de</NavLink></li>
+                                    <li className="pure-menu-item"><NavLink activeClassName="pure-menu-selected" className="pure-menu-link" to="license">Licencia</NavLink></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="App main">
+                        <div className="App-intro content">
+                            <Switch>
+                                <Route path="/search" component={Search} />
+                                <Route path="/validate" component={Validator} />
+                                <Route path="/download" component={Download} />
+                                <Route path="/about" component={About} />
+                                <Route path="/license" component={License} />
+                                <Route path="/"><Redirect to="/search" /></Route>
+                            </Switch>
 
-  render() {
-    return (
-      <div id="layout">
-        <a href="#menu" id="menuLink" className="menu-link">
-            <span></span>
-        </a>
-        <div id="menu">
-          <div className="pure-menu">
-              <span className="pure-menu-heading"></span>
-
-              <ul className="pure-menu-list">
-                  <li className="pure-menu-item"><IndexLink activeClassName="pure-menu-selected" className="pure-menu-link" to="/">Buscador</IndexLink></li>
-                  <li className="pure-menu-item"><Link activeClassName="pure-menu-selected" className="pure-menu-link" to="validate">Validador</Link></li>
-                  <li className="pure-menu-item"><Link activeClassName="pure-menu-selected" className="pure-menu-link" to="download">Descargar</Link></li>
-              </ul>
-
-              <div className="bottom-menu">
-                <ul className="pure-menu-list">
-                  <li className="pure-menu-item menu-item-divided"><Link activeClassName="pure-menu-selected" className="pure-menu-link" to="about">Acerca de</Link></li>
-                  <li className="pure-menu-item"><Link activeClassName="pure-menu-selected" className="pure-menu-link" to="license">Licencia</Link></li>
-                </ul>
-              </div>
-          </div>
-        </div>
-        <div className="App main">
-          <div className="App-intro content">
-            {/*
-              next we replace `<Child>` with `this.props.children`
-              the router will figure out the children for us
-            */}
-            {this.props.children}
-          </div>
-        </div>
-      </div>
-    )
-  }
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 
 }
--- client/src/validator/Validator.js
+++ client/src/validator/Validator.js
@@ -1,62 +1,63 @@
 import React, {
-  Component
+    Component
 }
-from 'react';
+    from 'react';
 import './Validator.css';
 import DigitGenerator from './DigitGenerator';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import EmbeddedGist from '../util/EmbeddedGist.js';
-var Config = require('../Config.js');
+import 'react-tabs/style/react-tabs.css';
+import { Config } from '../Config';
 
 class Validator extends Component {
 
-  getValues = (ruc) => {
-    return {
-      ruc: ruc,
-      digit: new DigitGenerator().getDigitoVerificadorBase11(ruc)
+    getValues = (ruc) => {
+        return {
+            ruc: ruc,
+            digit: new DigitGenerator().getDigitoVerificadorBase11(ruc)
+        };
+    }
+
+    changeCurrentRuc = (evt) => {
+        this.setState(this.getValues(evt.target.value));
     };
-  }
 
-  changeCurrentRuc = (evt) => {
-    this.setState(this.getValues(evt.target.value));
-  };
+    state = this.getValues(Config.EXAMPLE_RUC);
 
-  state = this.getValues(Config.EXAMPLE_RUC);
+    render() {
+        return (
+            <div>
+                <div className="header">
+                    <h1>Digitos verificadores</h1>
+                    <h2>Verificación rápida de RUCs</h2>
+                </div>
+                <form className="pure-form">
+                    <p>
+                        <label>El dígito verificador de <input
+                            className="ruc-input"
+                            placeholder={"1123456, 01-2140"}
+                            type="text"
+                            value={this.state.ruc}
+                            onChange={this.changeCurrentRuc} /> es <b>{this.state.digit}</b></label>
+                    </p>
+                </form>
 
-  render() {
-    return (
-      <div>
-        <div className="header">
-          <h1>Digitos verificadores</h1>
-          <h2>Verificación rápida de RUCs</h2>
-        </div>
-        <form className="pure-form">
-          <p>
-            <label>El dígito verificador de <input 
-              className="ruc-input"
-              placeholder={"1123456, 01-2140"} 
-              type="text" 
-              value={this.state.ruc}
-              onChange={this.changeCurrentRuc} /> es <b>{this.state.digit}</b></label>
-          </p>
-        </form>
-
-        <h2> Codigo fuente: </h2>
-        <Tabs>
-          <TabList>
-            <Tab>Java</Tab>
-            <Tab>JavaScript</Tab>
-          </TabList>
-          <TabPanel>
-            <EmbeddedGist gist="aVolpe/fffbe6a9e9858c7e3546fb1d55782152" file="SetUtils.java"></EmbeddedGist>
-          </TabPanel>
-          <TabPanel>
-            <EmbeddedGist gist="aVolpe/fffbe6a9e9858c7e3546fb1d55782152" file="DigitGenerator.js"></EmbeddedGist>
-          </TabPanel>
-        </Tabs>
-      </div>
-    );
-  }
+                <h2> Codigo fuente: </h2>
+                <Tabs>
+                    <TabList>
+                        <Tab>Java</Tab>
+                        <Tab>JavaScript</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <EmbeddedGist gist="aVolpe/fffbe6a9e9858c7e3546fb1d55782152" file="SetUtils.java" />
+                    </TabPanel>
+                    <TabPanel>
+                        <EmbeddedGist gist="aVolpe/fffbe6a9e9858c7e3546fb1d55782152" file="DigitGenerator.js" />
+                    </TabPanel>
+                </Tabs>
+            </div>
+        );
+    }
 }
 
 export default Validator;
